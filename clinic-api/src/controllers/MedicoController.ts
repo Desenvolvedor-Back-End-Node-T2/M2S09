2693c378c1eb8f4c7c852c4a5f68ea62ff41e9e4
Davi Saldanha <davisaldanha.ti@gmail.com>
2026-09-22 16:00:19 -0300

    refactor: substitui o repositório de médicos por MedicoService no MedicoController

--- clinic-api/src/controllers/MedicoController.ts
+++ clinic-api/src/controllers/MedicoController.ts
@@ -1,34 +1,19 @@
-import {Request, Response} from "express"
-import { AppDataSource } from "../data-source";
-import { Medico } from '../entities/Medico'
-import { MedicoResponseDTO } from "../dtos/usuario/MedicoResponseDTO";
+// src/controllers/MedicoController.ts
+import { Request, Response } from 'express';
 
-const medicoRepository = AppDataSource.getRepository(Medico)
+import { TypeOrmMedicoRepository } from '../repositories/typeorm/TypeOrmMedicoRepository';
+import { MedicoService } from '../service/MedicoService';
 
-export class MedicoController{
-    //GET /medicos
-    async listar(req: Request, res: Response){
-        const medicos = await medicoRepository.find()
+const medicoService = new MedicoService(new TypeOrmMedicoRepository());
 
-        const resposta = medicos.map(p => ({
-            nome: p.usuario.nome,
-            email: p.usuario.email,
-            especialidade: new MedicoResponseDTO(p).especialidade
-        }))
+export class MedicoController {
+  async listar(req: Request, res: Response) {
+    const medicos = await medicoService.listarTodos();
+    return res.json(medicos);
+  }
 
-        return res.json(resposta)
-    }
-
-    //GET /medicos/me
-    async meuPerfil(req: Request, res: Response){
-        const medico = await medicoRepository.findOne({
-            where: { usuario: {id: req.usuario?.sub}}
-        })
-
-        if(!medico){
-            return res.status(404).json({ erro: "Médico não encontrado."})
-        }
-
-        return res.json(medico)
-    }
+  async meuPerfil(req: Request, res: Response) {
+    const medico = await medicoService.buscarMeuPerfil(req.usuario!.sub);
+    return res.json(medico);
+  }
 }
