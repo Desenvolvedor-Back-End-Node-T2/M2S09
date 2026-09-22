32567aefd48652075b768e6cfeefd840fb790e05
Davi Saldanha <davisaldanha.ti@gmail.com>
2026-09-22 19:09:09 -0300

    feat: adiciona error handler middleware no servidor

--- clinic-api/src/server.ts
+++ clinic-api/src/server.ts
@@ -6,12 +6,14 @@ import { AppDataSource } from './data-source'
 import { routes } from './routes';
 import { swaggerSpec } from './config/swagger';
 import swaggerUi from 'swagger-ui-express'
+import { errorHandler } from './middleware/errorHandler';
 
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+app.use(errorHandler);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
