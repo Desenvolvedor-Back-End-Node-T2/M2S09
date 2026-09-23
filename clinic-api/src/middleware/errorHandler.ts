0dacebfde7a0a6f0e065257d4354e06a5a023119
Davi Saldanha <davisaldanha.ti@gmail.com>
2026-09-23 15:25:41 -0300

    feat: adiciona logging de erros utilizando Winston no middleware de tratamento de erros

--- clinic-api/src/middleware/errorHandler.ts
+++ clinic-api/src/middleware/errorHandler.ts
@@ -1,8 +1,10 @@
 import { Request, Response, NextFunction } from 'express'
 import { AppError } from '../errors/AppError'
+import { logger } from '../config/logger';
 
 export function errorHandler(err: Error, req: Request, res: Response, next: NextFunction){
     if (err instanceof AppError){
+        logger.error(err.message, { stack: err.stack, path: req.originalUrl})
         return res.status(err.statusCode).json({error: err.message})
     }
 
